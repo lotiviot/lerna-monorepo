83c86b200949516351d82ef7e743404fbf178220
Tristan Lotivio <109969233+tristan-lotivio-pilot@users.noreply.github.com>
2023-03-31 12:18:11 -0400

    commit that updates the version to allow for lerna versioning

---
+++ packages/project1/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { Component } from 'pfj-component-dev';
+</script>
+
+<Component/>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
